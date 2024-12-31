8f5c13d0e9243253fddb87af6fd795ad7f35f91e
Enzo Muzi <enzshadows@gmail.com>
2024-12-31 00:21:23 -0300

    Terceira parte 20% finish

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,61 +1,115 @@
 import { useEffect, useState } from "react";
-import { View, Text, Alert } from "react-native";
+import { View, Text, Alert, ActivityIndicator } from "react-native";
 
-import { api } from "@/services/api"
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
-import { Categories, CategoriesProps } from "@/components/categories"
+import { api } from "@/services/api";
+
+import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
 
-type MarketProps = PlaceProps
+type MarketProps = PlaceProps;
+
+type LocationType = {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+};
 
 export default function Home() {
-    const [categories, setCategories] = useState<CategoriesProps>([])
-    const [category, setCategory] = useState("")
-    const [markets, setMarkets] = useState<MarketProps[]>([])
+  const [categories, setCategories] = useState<CategoriesProps>([]);
+  const [category, setCategory] = useState("");
+  const [markets, setMarkets] = useState<MarketProps[]>([]);
+  const [currentLocation, setCurrentLocation] = useState<LocationType | null>(
+    null
+  );
+  const [loadingLocation, setLoadingLocation] = useState(true);
 
-        async function fetchCategories() {
-            try {
-                const { data } = await api.get("/categories")
-                setCategories(data)
-                setCategory(data[0].id)
-            } catch (error) {
-                console.log(error)
-                Alert.alert("Categorias", "Não foi possível carregar as categorias.")
-            }
-        }
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories");
+      setCategories(data);
+      setCategory(data[0].id);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.");
+    }
+  }
 
-        async function fetchMarkets() {
-            try {
-                if(!category) {
-                    return
-                }
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return;
+      }
 
-                const {data} = await api.get("/markets/category/" + category)
-                setMarkets(data)
-            } catch (error) {
-                console.log(error)
-                Alert.alert("Locais", "Não foi possível carregar os locais.")
-            }
-        }
+      const { data } = await api.get("/markets/category/" + category);
+      setMarkets(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Locais", "Não foi possível carregar os locais.");
+    }
+  }
 
-        useEffect(() => {
-            fetchCategories( )
-        }, [])
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
 
-        useEffect(() => {
-            fetchMarkets()
-        }, [category])
+      if (granted) {
+        const { coords } = await Location.getCurrentPositionAsync();
+        setCurrentLocation({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        });
+      } else {
+        Alert.alert(
+          "Permissão Negada",
+          "Não foi possível obter a localização."
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Houve um problema ao obter a localização.");
+    } finally {
+      setLoadingLocation(false);
+    }
+  }
 
+  useEffect(() => {
+    getCurrentLocation();
+    fetchCategories();
+  }, []);
 
-    return <View style={{flex: 1}}>
-        <Categories
+  useEffect(() => {
+    fetchMarkets();
+  }, [category]);
+
+  if (loadingLocation) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Carregando localização...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Categories
         data={categories}
         onSelect={setCategory}
         selected={category}
-        />
+      />
 
-        <Places data={markets}/>
+      {currentLocation && (
+        <MapView style={{ flex: 1 }} initialRegion={currentLocation} />
+      )}
+
+      <Places data={markets} />
     </View>
-    
+  );
 }
